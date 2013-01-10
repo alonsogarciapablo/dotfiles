e47097927898ab5da118d536021ce18829bebc91
Pablo Alonso Garcia <pabloalonso@dhcp-172-27-100-114.mtv.corp.google.com>
2013-01-10 11:35:47 -0800

    Install and uninstall scripts

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dot_files=(
+  ".ackrc"
+  ".bash_profile"
+  ".git-completion.bash"
+  ".git-prompt.sh"
+  ".gitconfig"
+  ".gitignore"
+  ".vim"
+  ".vimrc"
+)
+
+echo 'Copying dotfiles'
+echo '================'
+
+for file in "${dot_files[@]}"
+do
+  ln -s -F $PWD/$file $HOME/$file
+  echo "Linked $file"
+done
+
+echo '================'
+
+git submodule init
+
---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dot_files=(
+  ".ackrc"
+  ".bash_profile"
+  ".git-completion.bash"
+  ".git-prompt.sh"
+  ".gitconfig"
+  ".gitignore"
+  ".vim"
+  ".vimrc"
+)
+
+echo 'Removing dotfiles'
+echo '================'
+
+for file in "${dot_files[@]}"
+do
+  rm -rf $PWD/$file $HOME/$file
+  echo "Removed $HOME/$file"
+done
+
+echo '================'
+
